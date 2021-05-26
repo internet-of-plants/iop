6288ef63a55454e9adb86ed40b5332732fc02622
Paulo <paulosanz@poli.ufrj.br>
2021-05-26 01:09:08 -0300

    Improve base64 test

--- test/utils.cpp
+++ test/utils.cpp
@@ -7,6 +7,7 @@ int main(int argc, char** argv) {
     const std::string data = "asdasdasdadsasdasdas";
     const std::string expectedHash = "YXNkYXNkYXNkYWRzYXNkYXNkYXM=";
     const std::string hash = utils::base64Encode(reinterpret_cast<const uint8_t*>(data.c_str()), data.length()).c_str();
+    TEST_ASSERT_EQUAL(expectedHash.length(), hash.length());
     TEST_ASSERT_EQUAL_CHAR_ARRAY(hash.c_str(), expectedHash.c_str(), std::min(hash.length(), expectedHash.length()));
     UNITY_END();
     return 0;
